19d1ca7e315c48fac328177a997723ede3c65a7a
darlansaulo <darlansaulo@gmail.com>
2024-01-14 15:38:52 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+
+  state = {
+    post: {
+      id: 1,
+      title: 'Aprendendo'
+    }
+  }
+
+  render() {
+    return (
+    <div className="App">
+
+    </div>
+    );
+  }
+}
+
+export default App;
