a01804dcfc1654771a47cb5f95943ca3da920c33
darlansaulo <darlansaulo@gmail.com>
2024-01-21 22:25:18 -0300

    Primeiras funcionalidades: Um componente de classe mexendo com estado em React. Foi estilizado o componente e adicionado as funcionalidades de onClick e setando um novo estato.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-
   state = {
-    post: {
-      id: 1,
-      title: 'Aprendendo'
-    }
-  }
+    name: "Darlan",
+    counter: 0,
+  };
+
+  handleChangeNameEndCounterClick = () => {
+    const { counter, name } = this.state;
+
+    name === "Darlan"
+      ? this.setState({ name: "Junior", counter: counter + 1 })
+      : this.setState({ counter: counter + 1 });
+  };
+
+  handleRClick = () => {
+    this.setState({ counter: 0, name: "Darlan" });
+  };
 
   render() {
-    return (
-    <div className="App">
+    const { name, counter } = this.state;
 
-    </div>
+    return (
+      <div className="App">
+        <p className="nomeVariavel">
+          {name} {counter}
+        </p>
+        <button
+          className="bt-click"
+          onClick={this.handleChangeNameEndCounterClick}
+        >
+          CLICK
+        </button>
+        <button className="bt-reset" onClick={this.handleRClick}>
+          RESET
+        </button>
+      </div>
     );
   }
 }
