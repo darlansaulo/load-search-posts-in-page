a37f5c377dbfa6dab3e184e66d4149976f82b931
darlansaulo <darlansaulo@gmail.com>
2024-01-23 18:03:59 -0300

    Update: Adicionando uma nova requisição fetch para a API jsonplaceholder para buscar um array de foto e inserido no jsx para ser montado na tela. Também foi realizado a estilização do elemento montado.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ class App extends Component {
   }
 
   loadPosts = async () => {
-    const postsResp = fetch("https://jsonplaceholder.typicode.com/posts");
-    const [posts] = await Promise.all([postsResp]);
-    const postsJson = await posts.json();
+    const postsResp = fetch("https://jsonplaceholder.typicode.com/posts"); // API pública para teste / Public API for test
+    const photosResp = fetch("https://jsonplaceholder.typicode.com/photos");
 
-    this.setState({ posts: postsJson });
+    const [posts, photos] = await Promise.all([postsResp, photosResp]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postAndPhotos = postsJson.map((post, index) => {
+      return { ...post, photos: photosJson[index].url }
+    });
+
+    this.setState({ posts: postAndPhotos });
   };
 
   componentDidUpdate() {}
@@ -29,6 +37,7 @@ class App extends Component {
         <div className="posts">
           {posts.map((post) => (
             <div className="post">
+              <img src={post.photos} alt={post.title} className="photo-content"></img>
               <div key={post.id} className="post-content">
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
